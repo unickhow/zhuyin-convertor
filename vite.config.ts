f2f3aaf5482b506d705b8692bb8a681085f36bb7
unickhow <unick.zhow@gmail.com>
2025-07-18 20:51:52 +0800

    💡 feat: add clarity script

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,40 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      {
+        name: 'html-transform',
+        order: 'pre',
+        transformIndexHtml (html) {
+          if (mode === 'production') {
+            html = html.replace(
+              '</head>',
+              `<script type="text/javascript">
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "sgu02828dz");
+</script></head>`
+            )
+          }
+          return html
+        }
+      }
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      }
     }
   }
 })
